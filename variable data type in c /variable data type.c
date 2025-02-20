ed4f7fa96a0977a5f70512d4b805d51a71725441
imshivamchoudhary <shivamchoudhary012@gmail.com>
2025-02-20 23:42:04 +0530

    initial commit

--- variable data type in c /variable data type.c
+++ variable data type in c /variable data type.c
@@ -1,3 +1,14 @@
 // Variables
 //A name given to a memory location
 //declared by writing ----> int a,b,c;
+#include <stdio.h>
+int main() {
+  int a,b,c;
+ printf("Enter a number: ");
+ scanf("%d", &a);
+ printf("Enter a number: ");
+ scanf("%d", &b);
+ printf("The value of a is %d\n", a);
+ c=a+b;
+ printf("the sum is %d\n", c);
+}
